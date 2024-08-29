ce8678e759cc629dd5ce58da706a73308a6dab6b
Jannick <jannick.schroer@gmail.com>
2024-08-29 23:53:28 +0200

    Added Image, Title and Buttons to Appbar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,33 @@
-<template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+<script lang="ts" setup>
+import {useServerStore} from "@/store/Servers";
+import {storeToRefs} from "pinia";
+import {IMAGE_COUNT} from "@/config/config";
 
-      Essentials Preset
+const serverStore = useServerStore();
+const { cycleRunning } = storeToRefs(serverStore);
+const { startUpdateCycle, stopUpdateCycle } = serverStore;
+</script>
+
+
+<template>
+  <v-app-bar>
+    <v-img
+      :src="`/imgs/${Math.floor(Math.random() * IMAGE_COUNT) + 1}.png`"
+      aspect-ratio="1"
+      class="mx-2"
+      max-height="64"
+      max-width="64"
+    />
+    <v-app-bar-title
+      class="text-h5"
+    >
+      Server Interface
     </v-app-bar-title>
+    <v-spacer />
+    <v-btn
+      :icon="cycleRunning ? 'mdi-sync' : 'mdi-sync-off'"
+      @click="serverStore.cycleRunning ? stopUpdateCycle() : startUpdateCycle()"
+    />
   </v-app-bar>
 </template>
 
-<script lang="ts" setup>
-  //
-</script>
