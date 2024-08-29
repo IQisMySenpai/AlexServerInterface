9a913d9bb71240273b0091057ea08bf4b276b3fc
Jannick <jannick.schroer@gmail.com>
2024-08-29 23:53:46 +0200

    Server Grid in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,28 @@
-<template>
-  <HelloWorld />
-</template>
-
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import {useServerStore} from "@/store/Servers";
+import {storeToRefs} from "pinia";
+import ServerCard from "@/components/ServerCard.vue";
+
+const serverStore = useServerStore();
+const { servers } = storeToRefs(serverStore);
+
 </script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        v-for="(server, index) in servers"
+        :key="index"
+      >
+        <ServerCard
+          :server="server"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
